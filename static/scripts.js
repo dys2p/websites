b55cca949dc885a7e8cad83b42508e602c9c520c
d2ro <78594032+d2ro@users.noreply.github.com>
2024-08-13 17:45:57 +0200

    refactor: d-js-none, d-nojs-none

--- static/scripts.js
+++ static/scripts.js
@@ -14,7 +14,7 @@ function scheduleReload() {
 	}, 5*60*1000); // 5 minutes
 }
 
-// HTML noscript tag does not work with NoScript browser addon, so we apply class noscript and then call this function
-function removeNoscript() {
-	document.querySelectorAll(".noscript").forEach(e => e.remove());
-}
+document.addEventListener('DOMContentLoaded', function() {
+	document.querySelectorAll(".d-js-none").forEach(e => e.remove());
+	document.querySelectorAll(".d-nojs-none").forEach(e => e.classList.remove("d-nojs-none")); // style.css: .d-nojs-none { display: none; }
+});
